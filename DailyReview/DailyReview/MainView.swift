824c1ac3bfd2c83783666eec2e346227836e27ee
yubin425 <127329882+yubin425@users.noreply.github.com>
2024-10-29 11:07:56 +0900

    init view 추가
    필요한 뷰들 추가, 단순 추가로 내용 x

--- DailyReview/DailyReview/MainView.swift
+++ DailyReview/DailyReview/MainView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import SwiftData
 
-struct ContentView: View {
+struct MainView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
 
@@ -56,6 +56,6 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    MainView()
         .modelContainer(for: Item.self, inMemory: true)
 }
---
+++ DailyReview/DailyReview/ReviewView.swift
@@ -0,0 +1,18 @@
+//
+//  ReviewView.swift
+//  DailyReview
+//
+//  Created by 임유빈 on 10/29/24.
+//
+
+import SwiftUI
+
+struct ReviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReviewView()
+}
