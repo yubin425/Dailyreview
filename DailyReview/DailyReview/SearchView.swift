81085962ae225980d8477e69bc83b792434db64a
KIMDONGHA778 <baount@hanyang.ac.kr>
2024-10-30 11:00:36 +0900

    Create SearchView.swift

---
+++ DailyReview/DailyReview/SearchView.swift
@@ -0,0 +1,27 @@
+//
+//  SearchView.swift
+//  DailyReview
+//
+//  Created by 2022049898 on 10/29/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct ContentView: View {
+   // @State private var departmentId: Department.ID?
+   // @State private var productId: Product.ID?
+    @State private var searchText: String = ""
+
+
+    var body: some View {
+        NavigationSplitView {
+            //DepartmentList(departmentId: $departmentId)
+        } content: {
+            //ProductList(departmentId: departmentId, productId: $productId)
+        } detail: {
+            //ProductDetails(productId: productId)
+        }
+        .searchable(text: $searchText) // Adds a search field.
+    }
+}
