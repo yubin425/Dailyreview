c630c5e9b66a9275a72ef533429de5bd4916cb37
KIMDONGHA778 <baount@hanyang.ac.kr>
2024-11-05 13:40:41 +0900

    Update SharingView.swift

--- DailyReview/DailyReview/SharingView.swift
+++ DailyReview/DailyReview/SharingView.swift
@@ -8,10 +8,58 @@
 import SwiftUI
 
 struct SharingView: View {
+    @State private var screenshotImage: UIImage?
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button(action: {
+                takeScreenshot()
+            }) {
+                Text("Capture Screenshot")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+
+            Image("poster1")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: 300)
+                .padding()
+            
+            if let screenshotImage = screenshotImage {
+                Image(uiImage: screenshotImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                    .padding()
+            }
+        }
+    }
+
+    func takeScreenshot() {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let window = windowScene.windows.first else {
+            return
+        }
+
+        let scale = UIScreen.main.scale
+        UIGraphicsBeginImageContextWithOptions(window.bounds.size, false, scale)
+        if let context = UIGraphicsGetCurrentContext() {
+            window.layer.render(in: context)
+            let screenshot = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+
+            if let screenshot = screenshot {
+                DispatchQueue.main.async {
+                    self.screenshotImage = screenshot
+                }
+            }
+        }
     }
 }
+
 
 #Preview {
     SharingView()
